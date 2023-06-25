8ca56572ef0205631731b0e48621224827a7047a
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-06-25 11:28:31 -0500

    chore: Fix header log's timestamp

--- src/cli/utils/printHeader.ts
+++ src/cli/utils/printHeader.ts
@@ -3,5 +3,6 @@ import { version } from '../..';
 import { consola } from 'consola';
 
 export function printHeader() {
-  consola.log(`\n${pc.gray('WXT')} ${pc.gray(pc.bold(version))}`);
+  console.log();
+  consola.log(`${pc.gray('WXT')} ${pc.gray(pc.bold(version))}`);
 }
