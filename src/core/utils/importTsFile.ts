b462a6e1d8761586e9d717c0d765e7286cf6e45b
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-07-10 21:24:39 -0500

    WIP

--- src/core/utils/importTsFile.ts
+++ src/core/utils/importTsFile.ts
@@ -1,81 +1,52 @@
-import { consola } from 'consola';
 import createJITI from 'jiti';
-import transform from 'jiti/dist/babel';
+import { InternalConfig } from '../types';
 import { createUnimport } from 'unimport';
 import fs from 'fs-extra';
-import { getUnimportOptions } from './auto-imports';
-import { InternalConfig } from '../types';
-import { loadConfig } from 'c12';
 import { resolve } from 'path';
+import transform from 'jiti/dist/babel';
+import { getUnimportOptions } from './auto-imports';
 
 export async function importTsFile<T>(
   path: string,
   config: InternalConfig,
 ): Promise<T> {
-  const options = getUnimportOptions(config);
-  const unimport = createUnimport(options);
-  await unimport.scanImportsFromDir(undefined, { cwd: config.srcDir });
+  config.logger.log('Loading file metadata:', path);
+
+  const unimport = createUnimport({
+    ...getUnimportOptions(config),
+    // Only allow specific imports, not all from the project
+    imports: [{ name: '*', as: 'browser', from: 'webextension-polyfill' }],
+    dirs: [],
+  });
+  await unimport.init();
 
   const text = await fs.readFile(path, 'utf-8');
-  const res = await unimport.injectImports(text, path);
-  const transformedText = res.code;
+  const textNoImports = text.replace(/import.*[\n;]/gm, '');
+  const { code } = await unimport.injectImports(textNoImports);
+  config.logger.log(
+    ['Text:', text, 'No imports:', textNoImports, 'Code:', code].join('\n'),
+  );
 
-  // const jiti = createJITI(__filename, {
-  //   cache: false,
-  //   esmResolve: true,
-  //   interopDefault: true,
-  //   alias: {
-  //     'webextension-polyfill': '@webext-core/fake-browser',
-  //   },
-
-  //   transform(opts) {
-  //     if (opts.filename === path) opts.source = transformedText;
-
-  //     // Remove CSS imports from the source code - Jiti can't handle them.
-  //     opts.source = opts.source.replace(/^import ['"].*\.css['"];?$/gm, '');
-
-  //     // Call the default babel transformer with our modified source code
-  //     return transform(opts);
-  //   },
-  // });
-  // try {
-  //   return await jiti(path);
-  // } catch (err) {
-  //   consola.error(`Failed to import file: ${path}`);
-  //   throw err;
-  // }
+  const jiti = createJITI(__filename, {
+    cache: false,
+    esmResolve: true,
+    interopDefault: true,
+    alias: {
+      'webextension-polyfill': resolve(
+        config.root,
+        'node_modules/wxt/dist/virtual-modules/fake-browser.js',
+      ),
+    },
+    transform(opts) {
+      if (opts.filename === path) return transform({ ...opts, source: code });
+      else return transform(opts);
+    },
+  });
 
   try {
-    const loaded = await loadConfig<T>({
-      configFile: path,
-      rcFile: false,
-      jitiOptions: {
-        cache: false,
-        // esmResolve: true,
-        // interopDefault: true,
-        alias: {
-          'webextension-polyfill': resolve(
-            config.root,
-            'node_modules/wxt/dist/virtual-modules/fake-browser.js',
-          ),
-        },
-        transform(opts) {
-          if (opts.filename === path) opts.source = transformedText;
-
-          // Remove CSS imports from the source code - Jiti can't handle them.
-          opts.source = opts.source.replace(
-            /^import.*['"].*\.css['"];?$/gm,
-            '',
-          );
-
-          // Call the default babel transformer with our modified source code
-          return transform(opts);
-        },
-      },
-    });
-    return loaded.config;
+    return await jiti(path);
   } catch (err) {
-    consola.error(`Failed to import file: ${path}`);
+    config.logger.error(err);
     throw err;
   }
 }
