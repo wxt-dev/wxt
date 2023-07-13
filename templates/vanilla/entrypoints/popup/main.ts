132e103cbef0a913501c86455599be0a111214ef
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-07-13 17:50:39 -0500

    Add vanilla template

---
+++ templates/vanilla/entrypoints/popup/main.ts
@@ -0,0 +1,24 @@
+import './style.css';
+import typescriptLogo from '../../assets/typescript.svg';
+import viteLogo from '/wxt.svg';
+import { setupCounter } from '../../components/counter';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://wxt.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="WXT logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>WXT + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the WXT and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
