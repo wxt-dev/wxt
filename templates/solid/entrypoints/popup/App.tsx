4b4d2df669c41edc13955f216bfacdfeba40d3fc
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-07-22 08:37:29 -0500

    Follow Vite's solid template
    https://github.com/vitejs/vite/tree/main/packages/create-vite/template-solid

--- templates/solid/entrypoints/popup/App.tsx
+++ templates/solid/entrypoints/popup/App.tsx
@@ -1,22 +1,22 @@
-import { createSignal, type Component } from 'solid-js';
-
-import solidLogo from '../../assets/solid.svg';
+import { createSignal } from 'solid-js';
+import solidLogo from '@/assets/solid.svg';
+import wxtLogo from '/wxt.svg';
 import './App.css';
 
-const App: Component = () => {
+function App() {
   const [count, setCount] = createSignal(0);
 
   return (
     <>
       <div>
         <a href="https://wxt.dev" target="_blank">
-          <img src="/wxt.svg" class="logo" alt="WXT logo" />
+          <img src={wxtLogo} class="logo" alt="WXT logo" />
         </a>
-        <a href="https://www.solidjs.com/" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="SolidJS logo" />
+        <a href="https://solidjs.com" target="_blank">
+          <img src={solidLogo} class="logo solid" alt="Solid logo" />
         </a>
       </div>
-      <h1>WXT + SolidJS</h1>
+      <h1>WXT + Solid</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count()}
@@ -26,10 +26,10 @@ const App: Component = () => {
         </p>
       </div>
       <p class="read-the-docs">
-        Click on the WXT and SolidJS logos to learn more
+        Click on the WXT and Solid logos to learn more
       </p>
     </>
   );
-};
+}
 
 export default App;
