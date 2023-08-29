83e62a1d63a524cc62049ddc5dd3f3e3128b81e7
Aaron Klinker <aaronklinker1@gmail.com>
2023-08-29 15:55:44 -0500

    chore: Fix remote code E2E test

--- e2e/tests/remote-code.test.ts
+++ e2e/tests/remote-code.test.ts
@@ -3,7 +3,7 @@ import { TestProject } from '../utils';
 
 describe('Remote Code', () => {
   it('should download "url:*" modules and include them in the final bundle', async () => {
-    const url = 'https://stats.aklinker1.io/umami.js';
+    const url = 'https://code.jquery.com/jquery-3.7.1.slim.min.js';
     const project = new TestProject();
     project.addFile('entrypoints/popup.ts', `import "url:${url}"`);
 
@@ -12,7 +12,7 @@ describe('Remote Code', () => {
     const output = await project.serializeFile('.output/chrome-mv3/popup.js');
     expect(output).toContain(
       // Some text that will hopefully be in future versions of this script
-      'umami.disabled',
+      'jQuery v3.7.1',
     );
     expect(output).not.toContain(url);
     expect(
