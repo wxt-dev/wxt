36236907e1f99e2e8d2728a95f4dcda4809811a7
Aaron Klinker <aaronklinker1@gmail.com>
2023-09-05 09:21:00 -0500

    chore: Use import aliases in Vue template

--- templates/vue/entrypoints/popup/App.vue
+++ templates/vue/entrypoints/popup/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import HelloWorld from '../../components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <template>
@@ -8,7 +8,7 @@ import HelloWorld from '../../components/HelloWorld.vue';
       <img src="/wxt.svg" class="logo" alt="WXT logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="../../assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="WXT + Vue" />
