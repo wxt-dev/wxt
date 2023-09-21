76f6801e0fafee134efd3581063a09f8c17a556d
Aaron Klinker <aaronklinker1@gmail.com>
2023-09-21 12:11:31 -0500

    Add test case to demo

--- demo/src/entrypoints/overlay.content/index.ts
+++ demo/src/entrypoints/overlay.content/index.ts
@@ -2,9 +2,13 @@ import '../../common/style.css';
 
 export default defineContentScript({
   matches: ['*://*/*'],
-  async main() {
+  async main(ctx) {
     console.log(browser.runtime.id);
     logId();
+    const n = (Math.random() * 100).toFixed(1);
+    ctx.setInterval(() => {
+      console.log(n, browser.runtime.id);
+    }, 1e3);
     mountContentScriptUi();
   },
 });
