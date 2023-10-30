f8a0fb37a237169814d7034c0170aeedecfa8e3c
Aaron Klinker <aaronklinker1@gmail.com>
2023-10-29 20:24:27 -0500

    fix: Enable Vue SFC auto-imports in `vue` template

--- templates/vue/wxt.config.ts
+++ templates/vue/wxt.config.ts
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  imports: {
+    addons: {
+      vueTemplate: true,
+    },
+  },
   vite: () => ({
     plugins: [vue()],
   }),
