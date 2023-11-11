1880c37722c535d548ef7369ab3ea2d902542742
Aaron Klinker <aaronklinker1@gmail.com>
2023-11-11 16:15:27 -0600

    Convert from `zip-dir` to `archiver` for more control of the zipping process

--- src/core/zip.ts
+++ src/core/zip.ts
@@ -1,13 +1,26 @@
-import { InlineConfig } from '~/types';
-import zipdir from 'zip-dir';
-import { dirname, relative, resolve } from 'node:path';
+import archiver, { Archiver } from 'archiver';
+import { InlineConfig, InternalConfig } from '~/types';
+import { dirname, resolve } from 'node:path';
 import fs from 'fs-extra';
 import { kebabCaseAlphanumeric } from '~/core/utils/strings';
 import { getPackageJson } from '~/core/utils/package';
-import { minimatch } from 'minimatch';
 import { formatDuration } from '~/core/utils/time';
 import { printFileList } from '~/core/utils/log/printFileList';
 import { getInternalConfig, internalBuild } from '~/core/utils/building';
+// import archiver, { Archiver } from 'archiver';
+// import { InlineConfig, InternalConfig } from '~/types';
+// import { dirname, relative, resolve } from 'node:path';
+// import fs, { outputFile } from 'fs-extra';
+// import { kebabCaseAlphanumeric } from '~/core/utils/strings';
+// import { getPackageJson } from '~/core/utils/package';
+// import { minimatch } from 'minimatch';
+// import { formatDuration } from '~/core/utils/time';
+// import { printFileList } from '~/core/utils/log/printFileList';
+// import { getInternalConfig, internalBuild } from '~/core/utils/building';
+// import { glob } from 'fast-glob';
+// import path from 'node:path';
+// import { normalizePath } from './utils/paths';
+// import archiver, { Archiver } from 'archiver';
 
 /**
  * Build and zip the extension for distribution.
@@ -43,33 +56,35 @@ export async function zip(config?: InlineConfig): Promise<string[]> {
 
   const outZipFilename = applyTemplate(internalConfig.zip.artifactTemplate);
   const outZipPath = resolve(internalConfig.outBaseDir, outZipFilename);
-  await zipdir(internalConfig.outDir, {
-    saveTo: outZipPath,
-  });
+  await zipDir(internalConfig, internalConfig.outDir, outZipPath);
   zipFiles.push(outZipPath);
 
   // ZIP sources for Firefox
 
-  if (internalConfig.browser === 'firefox') {
-    const sourcesZipFilename = applyTemplate(
-      internalConfig.zip.sourcesTemplate,
-    );
-    const sourcesZipPath = resolve(
-      internalConfig.outBaseDir,
-      sourcesZipFilename,
-    );
-    await zipdir(internalConfig.zip.sourcesRoot, {
-      saveTo: sourcesZipPath,
-      filter(path) {
-        const relativePath = relative(internalConfig.zip.sourcesRoot, path);
-        const matchedPattern = internalConfig.zip.ignoredSources.find(
-          (pattern) => minimatch(relativePath, pattern),
-        );
-        return matchedPattern == null;
-      },
-    });
-    zipFiles.push(sourcesZipPath);
-  }
+  // if (internalConfig.browser === 'firefox') {
+  //   const sourcesZipFilename = applyTemplate(
+  //     internalConfig.zip.sourcesTemplate,
+  //   );
+  //   const sourcesZipPath = resolve(
+  //     internalConfig.outBaseDir,
+  //     sourcesZipFilename,
+  //   );
+  //   const files = await glob('**/*', {
+  //     cwd: internalConfig.root,
+  //     ignore: internalConfig.zip.ignoredSources,
+  //   });
+  //   await zipdir(internalConfig.zip.sourcesRoot, {
+  //     saveTo: sourcesZipPath,
+  //     filter(path) {
+  //       const relativePath = relative(internalConfig.zip.sourcesRoot, path);
+  //       const matchedPattern = internalConfig.zip.ignoredSources.find(
+  //         (pattern) => minimatch(relativePath, pattern),
+  //       );
+  //       return matchedPattern == null;
+  //     },
+  //   });
+  //   zipFiles.push(sourcesZipPath);
+  // }
 
   await printFileList(
     internalConfig.logger.success,
@@ -79,4 +94,36 @@ export async function zip(config?: InlineConfig): Promise<string[]> {
   );
 
   return zipFiles;
+}
+
+function zipDir(
+  config: InternalConfig,
+  directory: string,
+  outputPath: string,
+  additionalWork?: (archive: Archiver) => void,
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const archive = archiver('zip');
+
+    archive.on('warning', config.logger.warn);
+    archive.on('error', (err) => {
+      config.logger.error(err);
+      reject(err);
+    });
+    archive.on('close', () => resolve());
+
+    // Create output stream
+    const outputStream = fs.createWriteStream(outputPath);
+    archive.pipe(outputStream);
+
+    // Add files
+    archive.glob('**/*', {
+      cwd: directory,
+      skip: ['**/node_modules', '**/.git'],
+    });
+
+    additionalWork?.(archive);
+
+    archive.finalize().then(resolve);
+  });
 }
