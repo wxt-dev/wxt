60d6707b113a63acfe41faf07680922801a47c9c
Aaron <aaronklinker1@gmail.com>
2023-11-26 11:36:42 -0600

    chore: Use `normalize-path` instead of `vite.normalizePath` (#244)

--- src/core/utils/paths.ts
+++ src/core/utils/paths.ts
@@ -1,19 +1,19 @@
-import nodePath from 'node:path';
-import * as vite from 'vite';
+import systemPath from 'node:path';
+import normalize from 'normalize-path';
 
 /**
  * Converts system paths to normalized bundler path. On windows and unix, this returns paths with /
  * instead of \.
  */
 export function normalizePath(path: string): string {
-  return vite.normalizePath(path);
+  return normalize(path);
 }
 
 /**
  * Given a normalized path, convert it to the system path style. On Windows, switch to \, otherwise use /.
  */
 export function unnormalizePath(path: string): string {
-  return nodePath.normalize(path);
+  return systemPath.normalize(path);
 }
 
 export const CSS_EXTENSIONS = ['css', 'scss', 'sass', 'less', 'styl', 'stylus'];
