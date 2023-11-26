4be3b3402547ea7cc33bb5e0a3bf13b951042f67
Aaron Klinker <aaronklinker1@gmail.com>
2023-11-26 08:57:00 -0600

    Use import alias

--- src/core/builders/vite/plugins/globals.ts
+++ src/core/builders/vite/plugins/globals.ts
@@ -1,6 +1,6 @@
 import * as vite from 'vite';
 import { InternalConfig } from '~/types';
-import { getGlobals } from '../../../utils/globals';
+import { getGlobals } from '~/core/utils/globals';
 
 export function globals(config: InternalConfig): vite.PluginOption {
   return {
