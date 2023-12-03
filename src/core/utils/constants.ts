489314b7414aed45db152b35ff4cddb08cbe2b06
Aaron Klinker <aaronklinker1@gmail.com>
2023-12-03 13:35:28 -0600

    Cleanup todos

---
+++ src/core/utils/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Module ID used to build the background in dev mode if the extension doesn't include a background
+ * script/service worker.
+ */
+export const VIRTUAL_NOOP_BACKGROUND_MODULE_ID = 'virtual:user-background';
