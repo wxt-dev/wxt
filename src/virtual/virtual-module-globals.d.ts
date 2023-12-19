ae86592f10ebb00fbe36c2ec484a053fb5437c21
Aaron Klinker <aaronklinker1@gmail.com>
2023-12-19 11:37:47 -0600

    Fix compile check

--- src/virtual/virtual-module-globals.d.ts
+++ src/virtual/virtual-module-globals.d.ts
@@ -1,21 +1,39 @@
 // Types required to make the virtual modules happy.
 
 declare module 'virtual:user-background' {
-  const definition: import('wxt/types').BackgroundDefinition;
+  const definition: { main: () => void };
   export default definition;
 }
 
 declare module 'virtual:user-content-script-isolated-world' {
-  const definition: import('wxt/types').ContentScriptIsolatedWorldDefinition;
+  const definition: {
+    main: (
+      ctx: import('wxt/client').ContentScriptContext,
+    ) => void | Promise<void>;
+  };
   export default definition;
 }
 
 declare module 'virtual:user-content-script-main-world' {
-  const definition: import('wxt/types').ContentScriptMainWorldDefinition;
+  const definition: { main: () => void | Promise<void> };
   export default definition;
 }
 
 declare module 'virtual:user-unlisted-script' {
-  const definition: import('wxt/types').UnlistedScriptDefinition;
+  const definition: { main: () => void | Promise<void> };
   export default definition;
+}
+
+declare module 'wxt/browser' {
+  export const browser: import('webextension-polyfill').Browser;
+}
+
+declare module 'wxt/client' {
+  export class ContentScriptContext {
+    constructor(name: string, options: any);
+  }
+}
+
+declare module 'wxt/testing' {
+  export const fakeBrowser: import('webextension-polyfill').Browser;
 }
