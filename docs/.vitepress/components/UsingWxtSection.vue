c1b5a9d270bafac071d5f07aeb89424a98dcc63a
Florian Metz <me@timeraa.dev>
2024-02-03 05:46:46 +0100

    docs: Add premid extension to homepage (#399)

--- docs/.vitepress/components/UsingWxtSection.vue
+++ docs/.vitepress/components/UsingWxtSection.vue
@@ -17,6 +17,7 @@ const chromeExtensionIds = [
   'okifoaikfmpfcamplcfjkpdnhfodpkil', // Doozy: Ai Made Easy
   'lknmjhcajhfbbglglccadlfdjbaiifig', // tl;dv - Record, Transcribe & ChatGPT for Google Meet
   'youtube中文配音/oglffgiaiekgeicdgkdlnlkhliajdlja', // Youtube中文配音
+  'agjnjboanicjcpenljmaaigopkgdnihi', // PreMiD
 ];
 
 const { data } = useListExtensionDetails(chromeExtensionIds);
