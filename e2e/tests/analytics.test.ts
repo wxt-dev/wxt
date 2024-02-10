a050a0f6a2d04227279474fdbb64b995e06f017f
Aaron Klinker <aaronklinker1@gmail.com>
2024-02-10 03:23:42 -0600

    Fix type error

--- e2e/tests/analytics.test.ts
+++ e2e/tests/analytics.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { TestProject } from '../utils';
 import { resetBundleIncrement } from '~/core/builders/vite/plugins';
 
