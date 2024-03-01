f558f080cc48c3d47988b7cbb5bf5418a919def9
Aaron Klinker <aaronklinker1@gmail.com>
2024-03-01 07:57:35 -0600

    Skip bun win32 tests

--- src/core/package-managers/__tests__/bun.test.ts
+++ src/core/package-managers/__tests__/bun.test.ts
@@ -4,23 +4,26 @@ import { bun } from '../bun';
 
 const cwd = path.resolve(__dirname, 'fixtures/bun-project');
 
-describe('Bun Package Management Utils', () => {
-  describe('listDependencies', () => {
-    it('should list direct dependencies', async () => {
-      const actual = await bun.listDependencies({ cwd });
-      expect(actual).toEqual([
-        { name: 'flatten', version: '1.0.3' },
-        { name: 'mime-types', version: '2.1.35' },
-      ]);
-    });
+describe.skipIf(() => process.platform === 'win32')(
+  'Bun Package Management Utils',
+  () => {
+    describe('listDependencies', () => {
+      it('should list direct dependencies', async () => {
+        const actual = await bun.listDependencies({ cwd });
+        expect(actual).toEqual([
+          { name: 'flatten', version: '1.0.3' },
+          { name: 'mime-types', version: '2.1.35' },
+        ]);
+      });
 
-    it('should list all dependencies', async () => {
-      const actual = await bun.listDependencies({ cwd, all: true });
-      expect(actual).toEqual([
-        { name: 'flatten', version: '1.0.3' },
-        { name: 'mime-db', version: '1.52.0' },
-        { name: 'mime-types', version: '2.1.35' },
-      ]);
+      it('should list all dependencies', async () => {
+        const actual = await bun.listDependencies({ cwd, all: true });
+        expect(actual).toEqual([
+          { name: 'flatten', version: '1.0.3' },
+          { name: 'mime-db', version: '1.52.0' },
+          { name: 'mime-types', version: '2.1.35' },
+        ]);
+      });
     });
-  });
-});
+  },
+);
