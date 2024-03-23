37064584a79296d94268d840fe7de3ec820c260a
btea <2356281422@qq.com>
2024-03-23 10:25:27 +0800

    test: Add a test that the directory is not empty when bootstrapping the project

--- e2e/tests/init.test.ts
+++ e2e/tests/init.test.ts
@@ -1,7 +1,8 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { TestProject } from '../utils';
 import { execaCommand } from 'execa';
 import glob from 'fast-glob';
+import consola from 'consola';
 
 describe('Init command', () => {
   it('should download and create a template', async () => {
@@ -41,5 +42,21 @@ describe('Init command', () => {
         "wxt.config.ts",
       ]
     `);
+  });
+
+  it('should throw an error if the directory is not empty', async () => {
+    const project = new TestProject();
+
+    await execaCommand(`pnpm -s wxt init ${project.root} -t vue --pm npm`, {
+      env: { ...process.env, CI: 'true' },
+      stdio: 'ignore',
+    });
+
+    await expect(() =>
+      execaCommand(`pnpm -s wxt init ${project.root} -t vue --pm npm`, {
+        env: { ...process.env, CI: 'true' },
+        stdio: 'ignore',
+      }),
+    ).rejects.toThrowError('Command failed with exit code 1:');
   });
 });
