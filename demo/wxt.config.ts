1414c2fcff06321fd2ff5867d8260eced325d129
Aaron <aaronklinker1@gmail.com>
2024-04-03 13:50:32 -0500

    cleanup

--- demo/wxt.config.ts
+++ demo/wxt.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, scopedVitePlugin } from 'wxt';
+import { defineConfig } from 'wxt';
 
 export default defineConfig({
   srcDir: 'src',
@@ -18,14 +18,4 @@ export default defineConfig({
   analysis: {
     open: true,
   },
-  vite: () => ({
-    plugins: [
-      scopedVitePlugin(['popup', 'ui'], () => ({
-        name: 'TEST',
-        config() {
-          console.log('SCOPE APPLIED!');
-        },
-      })),
-    ],
-  }),
 });
