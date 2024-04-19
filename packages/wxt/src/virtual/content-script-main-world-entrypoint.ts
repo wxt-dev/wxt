b1ec6c0e6de747e07e012da45ead7d98af257d70
Aaron <aaronklinker1@gmail.com>
2024-04-19 16:25:17 -0500

    feat: Support returning values from scripts executed with the scripting API

--- packages/wxt/src/virtual/content-script-main-world-entrypoint.ts
+++ packages/wxt/src/virtual/content-script-main-world-entrypoint.ts
@@ -1,14 +1,20 @@
 import definition from 'virtual:user-content-script-main-world-entrypoint';
 import { logger } from '../sandbox/utils/logger';
 
-(async () => {
+const result = (async () => {
   try {
     const { main } = definition;
-    await main();
+    return await main();
   } catch (err) {
     logger.error(
       `The content script "${import.meta.env.ENTRYPOINT}" crashed on startup!`,
       err,
     );
+    throw err;
   }
 })();
+
+// Return the main function's result to the background when executed via the scripting API.
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/scripting/executeScript#return_value
+// Tested on both Chrome and Firefox
+result;
