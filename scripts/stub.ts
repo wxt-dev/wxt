df703d0706bf6923bb108fa46f83b274ffefdacd
Aaron <aaronklinker1@gmail.com>
2024-05-03 17:13:11 -0500

    stub for netlify

--- scripts/stub.ts
+++ scripts/stub.ts
@@ -1,5 +1,7 @@
 // This file is a stub just so netlify doesn't throw an error when building
 // docs. Once another TS file apart of the root project is added, this can be
 // removed.
+// If the website deploys on a PR branch with this file removed, you can safely
+// delete it.
 
 export {};
