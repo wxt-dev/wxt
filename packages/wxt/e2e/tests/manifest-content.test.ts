77bdb92cd278819df8d588d8aa364b919a6c7874
Aaron <aaronklinker1@gmail.com>
2024-05-04 10:41:57 -0500

    Add e2e test

--- packages/wxt/e2e/tests/manifest-content.test.ts
+++ packages/wxt/e2e/tests/manifest-content.test.ts
@@ -1,33 +1,36 @@
 import { describe, it, expect } from 'vitest';
 import { TestProject } from '../utils';
 
-describe('Manifest Content', () => {
-  it.each([
-    { browser: undefined, outDir: 'chrome-mv3', expected: undefined },
-    { browser: 'chrome', outDir: 'chrome-mv3', expected: undefined },
-    { browser: 'firefox', outDir: 'firefox-mv2', expected: true },
-    { browser: 'safari', outDir: 'safari-mv2', expected: false },
-  ])(
-    'should respect the per-browser entrypoint option with %j',
-    async ({ browser, expected, outDir }) => {
-      const project = new TestProject();
+describe.each([true, false])(
+  'Manifest Content (Vite runtime? %s)',
+  (viteRuntime) => {
+    it.each([
+      { browser: undefined, outDir: 'chrome-mv3', expected: undefined },
+      { browser: 'chrome', outDir: 'chrome-mv3', expected: undefined },
+      { browser: 'firefox', outDir: 'firefox-mv2', expected: true },
+      { browser: 'safari', outDir: 'safari-mv2', expected: false },
+    ])(
+      'should respect the per-browser entrypoint option with %j',
+      async ({ browser, expected, outDir }) => {
+        const project = new TestProject();
 
-      project.addFile(
-        'entrypoints/background.ts',
-        `export default defineBackground({
+        project.addFile(
+          'entrypoints/background.ts',
+          `export default defineBackground({
           persistent: {
             firefox: true,
             safari: false,
           },
           main: () => {},
         })`,
-      );
-      await project.build({ browser });
+        );
+        await project.build({ browser, experimental: { viteRuntime } });
 
-      const safariManifest = await project.getOutputManifest(
-        `.output/${outDir}/manifest.json`,
-      );
-      expect(safariManifest.background.persistent).toBe(expected);
-    },
-  );
-});
+        const safariManifest = await project.getOutputManifest(
+          `.output/${outDir}/manifest.json`,
+        );
+        expect(safariManifest.background.persistent).toBe(expected);
+      },
+    );
+  },
+);
