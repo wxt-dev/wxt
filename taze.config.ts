239d0ee01ae24dffea138e0626920524b09490f8
Aaron <aaronklinker1@gmail.com>
2024-06-13 15:13:53 -0500

    Fix types

--- taze.config.ts
+++ taze.config.ts
@@ -1,5 +1,4 @@
-import { defineConfig } from 'taze';
-
-export default defineConfig({
+// https://github.com/antfu-collective/taze?tab=readme-ov-file#config-file
+export default {
   exclude: ['typedoc', 'typedoc-plugin-markdown', 'typedoc-vitepress-theme'],
-});
+};
