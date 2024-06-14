771ea030698ba96059ed2d69a7b4097c14095f6b
Aaron <aaronklinker1@gmail.com>
2024-06-13 23:23:32 -0500

    feat: Add `@wxt-dev/module-solid` package (#730)

---
+++ packages/module-solid/src/index.ts
@@ -0,0 +1,27 @@
+import 'wxt';
+import { addImportPreset, addViteConfig, defineWxtModule } from 'wxt/modules';
+import solid, { Options as PluginOptions } from 'vite-plugin-solid';
+
+export default defineWxtModule<SolidModuleOptions>({
+  name: '@wxt-dev/module-solid',
+  configKey: 'solid',
+  setup(wxt, options) {
+    const { vite } = options ?? {};
+
+    addViteConfig(wxt, () => ({
+      plugins: [solid(vite)],
+    }));
+
+    addImportPreset(wxt, 'solid-js');
+  },
+});
+
+export interface SolidModuleOptions {
+  vite?: PluginOptions;
+}
+
+declare module 'wxt' {
+  export interface InlineConfig {
+    solid?: SolidModuleOptions;
+  }
+}
