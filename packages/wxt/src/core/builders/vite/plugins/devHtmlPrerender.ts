8432d73a6b298b223aac4df6c097b7639600cb97
Aaron <aaronklinker1@gmail.com>
2024-06-13 19:17:23 -0500

    fix: Generalize react-refresh preamble logic to virtualize all inline scripts (#728)

--- packages/wxt/src/core/builders/vite/plugins/devHtmlPrerender.ts
+++ packages/wxt/src/core/builders/vite/plugins/devHtmlPrerender.ts
@@ -4,9 +4,10 @@ import { getEntrypointName } from '~/core/utils/entrypoints';
 import { parseHTML } from 'linkedom';
 import { dirname, relative, resolve } from 'node:path';
 import { normalizePath } from '~/core/utils/paths';
+import { murmurHash } from 'ohash';
 
-// Cache the preamble script for all devHtmlPrerender plugins, not just one
-let reactRefreshPreamble = '';
+// Stored outside the plugin to effect all instances of the devHtmlPrerender plugin.
+const inlineScriptContents: Record<number, string> = {};
 
 /**
  * Pre-renders the HTML entrypoints when building the extension to connect to the dev server.
@@ -20,8 +21,9 @@ export function devHtmlPrerender(
     config.wxtModuleDir,
     'dist/virtual/reload-html.js',
   );
-  const virtualReactRefreshId = '@wxt/virtual-react-refresh';
-  const resolvedVirtualReactRefreshId = '\0' + virtualReactRefreshId;
+
+  const virtualInlineScript = 'virtual:wxt-inline-script';
+  const resolvedVirtualInlineScript = '\0' + virtualInlineScript;
 
   return [
     {
@@ -76,22 +78,22 @@ export function devHtmlPrerender(
         const serverHtml = await server.transformHtml(url, html, originalUrl);
         const { document } = parseHTML(serverHtml);
 
-        // React pages include a preamble as an unsafe-inline type="module" script to enable fast refresh, as shown here:
-        // https://github.com/wxt-dev/wxt/issues/157#issuecomment-1756497616
-        // Since unsafe-inline scripts are blocked by MV3 CSPs, we need to virtualize it.
-        const reactRefreshScript = Array.from(
-          document.querySelectorAll('script[type=module]'),
-        ).find((script) => script.innerHTML.includes('@react-refresh'));
-        if (reactRefreshScript) {
-          // Save preamble to serve from server
-          reactRefreshPreamble = reactRefreshScript.innerHTML;
+        // Replace inline script with virtual module served via dev server.
+        // Extension CSP blocks inline scripts, so that's why we're pulling them
+        // out.
+        const inlineScripts = document.querySelectorAll('script:not([src])');
+        inlineScripts.forEach((script) => {
+          // Save the text content for later
+          const textContent = script.textContent ?? '';
+          const hash = murmurHash(textContent);
+          inlineScriptContents[hash] = textContent;
 
           // Replace unsafe inline script
           const virtualScript = document.createElement('script');
           virtualScript.type = 'module';
-          virtualScript.src = `${server.origin}/${virtualReactRefreshId}`;
-          reactRefreshScript.replaceWith(virtualScript);
-        }
+          virtualScript.src = `${server.origin}/@id/${virtualInlineScript}?${hash}`;
+          script.replaceWith(virtualScript);
+        });
 
         // Change /@vite/client -> http://localhost:3000/@vite/client
         const viteClientScript = document.querySelector<HTMLScriptElement>(
@@ -112,18 +114,25 @@ export function devHtmlPrerender(
       name: 'wxt:virtualize-react-refresh',
       apply: 'serve',
       resolveId(id) {
-        if (id === `/${virtualReactRefreshId}`) {
-          return resolvedVirtualReactRefreshId;
+        // Resolve inline scripts
+        if (id.startsWith(virtualInlineScript)) {
+          return '\0' + id;
         }
-        // Ignore chunk contents when pre-rendering
+
+        // Ignore chunks during HTML file pre-rendering
         if (id.startsWith('/chunks/')) {
           return '\0noop';
         }
       },
       load(id) {
-        if (id === resolvedVirtualReactRefreshId) {
-          return reactRefreshPreamble;
+        // Resolve virtualized inline scripts
+        if (id.startsWith(resolvedVirtualInlineScript)) {
+          // id="virtual:wxt-inline-script?<hash>"
+          const hash = Number(id.substring(id.indexOf('?') + 1));
+          return inlineScriptContents[hash];
         }
+
+        // Ignore chunks during HTML file pre-rendering
         if (id === '\0noop') {
           return '';
         }
