515edec16eb954dcdfe8484879fcce471e88571a
Aaron <aaronklinker1@gmail.com>
2024-06-26 23:39:48 -0500

    revert cjs removal

--- packages/module-solid/build.config.ts
+++ packages/module-solid/build.config.ts
@@ -5,5 +5,8 @@ export default defineBuildConfig({
   rootDir: 'modules',
   outDir: resolve(__dirname, 'dist'),
   entries: [{ input: 'solid.ts', name: 'index' }],
+  rollup: {
+    emitCJS: true,
+  },
   declaration: true,
 });
