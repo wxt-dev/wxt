a36cc642e2ad80c1f56ec9bf1293c8c3ed07ee9f
Knight Yoshi <6134576+KnightYoshi@users.noreply.github.com>
2024-06-29 11:58:57 -0400

    feat: import eslint
    import('eslint') is not compatible `"type": "module"`

--- packages/wxt/src/core/utils/eslint.ts
+++ packages/wxt/src/core/utils/eslint.ts
@@ -1,15 +1,10 @@
-import { resolve } from 'node:path';
-import fs from 'fs-extra';
-import { wxt } from '../wxt';
-
 export async function getEslintVersion(): Promise<string[]> {
   try {
-    const packageJsonPath = resolve(
-      wxt.config.root,
-      'node_modules/eslint/package.json',
+    const require = (await import('node:module')).default.createRequire(
+      import.meta.url,
     );
-    const eslintPackageJson = await fs.readJson(packageJsonPath);
-    return eslintPackageJson.version?.split('.') ?? [];
+    const { ESLint } = require('eslint');
+    return ESLint.version?.split('.') ?? [];
   } catch (error) {
     return [];
   }
