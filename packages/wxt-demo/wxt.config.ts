ac9a4380b1dc3edcae5fc29f32225bd68629d59e
Aaron <aaronklinker1@gmail.com>
2024-07-24 15:14:25 -0500

    cleanup

--- packages/wxt-demo/wxt.config.ts
+++ packages/wxt-demo/wxt.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'wxt';
 
 export default defineConfig({
   srcDir: 'src',
-  // extensionApi: 'chrome',
+  extensionApi: 'chrome',
   manifest: {
     permissions: ['storage'],
     default_locale: 'en',
