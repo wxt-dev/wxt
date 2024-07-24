b1ef71a77b101556507a9dc556120b77da199a7e
Aaron <aaronklinker1@gmail.com>
2024-07-24 11:23:44 -0500

    Cleanup

--- packages/wxt/src/core/builders/vite/plugins/resolveExtensionApi.ts
+++ packages/wxt/src/core/builders/vite/plugins/resolveExtensionApi.ts
@@ -2,13 +2,15 @@ import { ResolvedConfig } from '../../../../types';
 import type * as vite from 'vite';
 
 /**
- * Apply the experimental config for which extension API is used.
+ * Apply the experimental config for which extension API is used. This only
+ * effects the extension API included at RUNTIME - during development, types
+ * depend on the import.
+ *
+ * NOTE: this only works if we import `wxt/browser` instead of using the relative path.
  */
 export function resolveExtensionApi(config: ResolvedConfig): vite.Plugin {
-  const polyfillVirtualId = 'virtual:wxt-webextension-polyfill-disabled';
-
   return {
-    name: 'wxt:exclude-browser-polyfill',
+    name: 'wxt:resolve-extension-api',
     config() {
       // Only apply the config if we're disabling the polyfill
       if (config.experimental.extensionApi === 'webextension-polyfill') return;
@@ -17,22 +19,9 @@ export function resolveExtensionApi(config: ResolvedConfig): vite.Plugin {
         resolve: {
           alias: [
             { find: /^wxt\/browser$/, replacement: 'wxt/browser/chrome' },
-            // {
-            //   find: 'webextension-polyfill',
-            //   replacement: polyfillVirtualId,
-            // },
           ],
         },
       };
     },
-    // load(id) {
-    //   console.log(id);
-    // },
-    // load(id) {
-    //   if (id === polyfillVirtualId) {
-    //     // Use chrome instead of the polyfill when disabled.
-    //     return 'export default chrome';
-    //   }
-    // },
   };
 }
