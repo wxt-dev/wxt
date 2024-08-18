041ef74081e2f3e47de00d140ea069e19bb67d9b
Aaron <aaronklinker1@gmail.com>
2024-08-18 15:23:10 -0500

    Reset changeg

--- packages/wxt/src/core/utils/building/generate-wxt-dir.ts
+++ packages/wxt/src/core/utils/building/generate-wxt-dir.ts
@@ -233,7 +233,11 @@ function getMainDeclarationEntry(references: WxtDirEntry[]): WxtDirFileEntry {
 
 async function getTsConfigEntry(): Promise<WxtDirFileEntry> {
   const dir = wxt.config.wxtDir;
-  const getTsconfigPath = (path: string) => normalizePath(relative(dir, path));
+  const getTsconfigPath = (path: string) => {
+    const res = normalizePath(relative(dir, path));
+    if (res.startsWith('.') || res.startsWith('/')) return res;
+    return './' + res;
+  };
   const paths = Object.entries(wxt.config.alias)
     .flatMap(([alias, absolutePath]) => {
       const aliasPath = getTsconfigPath(absolutePath);
