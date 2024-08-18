2731372295bf1ddf28e19884ece29b45f4699dab
Aaron <aaronklinker1@gmail.com>
2024-08-18 14:20:16 -0500

    Reload on save

--- packages/i18n/src/module.ts
+++ packages/i18n/src/module.ts
@@ -7,10 +7,12 @@ import {
 } from './build';
 import glob from 'fast-glob';
 import { basename, extname, join, resolve } from 'node:path';
+import { watch } from 'chokidar';
+import { GeneratedPublicFile, WxtDirFileEntry } from 'wxt';
+import { writeFile } from 'node:fs/promises';
 
 export default defineWxtModule({
   name: 'wxt-builtin-i18n',
-  configKey: 'i18n',
   imports: [{ from: '#i18n', name: 'i18n' }],
 
   setup(wxt) {
@@ -35,11 +37,22 @@ export default defineWxtModule({
       }));
     };
 
-    // Create .wxt/i18n.ts
+    const generateOutputJsonFiles = async (): Promise<
+      GeneratedPublicFile[]
+    > => {
+      const files = await getLocalizationFiles();
+      return await Promise.all(
+        files.map(async ({ file, locale }) => {
+          const messages = await parseMessagesFile(file);
+          return {
+            contents: generateChromeMessagesText(messages),
+            relativeDest: join('_locales', locale, 'messages.json'),
+          };
+        }),
+      );
+    };
 
-    const sourcePath = resolve(wxt.config.wxtDir, 'i18n.ts');
-
-    wxt.hooks.hook('prepare:types', async (wxt, entries) => {
+    const generateTypes = async (): Promise<WxtDirFileEntry> => {
       const files = await getLocalizationFiles();
       const defaultLocaleFile = files.find(
         ({ locale }) => locale === wxt.config.manifest.default_locale,
@@ -51,41 +64,93 @@ export default defineWxtModule({
       }
 
       const messages = await parseMessagesFile(defaultLocaleFile.file);
+      return {
+        path: typesPath,
+        text: generateTypeText(messages),
+      };
+    };
+
+    const updateLocalizations = async (file: string): Promise<void> => {
+      wxt.logger.info(
+        `\`[i18n]\` Localization file changed: \`${basename(file)}\``,
+      );
+
+      // Regenerate files
+      const [typesFile, jsonFiles] = await Promise.all([
+        generateTypes(),
+        generateOutputJsonFiles(),
+      ]);
+
+      // Write files to disk
+      await Promise.all([
+        writeFile(
+          resolve(wxt.config.wxtDir, typesFile.path),
+          typesFile.text,
+          'utf8',
+        ),
+        ...jsonFiles.map((file) =>
+          writeFile(
+            resolve(wxt.config.outDir, file.relativeDest),
+            file.contents,
+            'utf8',
+          ),
+        ),
+      ]);
+
+      // TODO: Implement HMR instead of reloading extension. The reload is
+      // fast, but it causes the popup to close, which I'd like to prevent.
+      wxt.server?.reloadExtension();
+      wxt.logger.success(`\`[i18n]\` Extension reloaded`);
+    };
+
+    // Create .wxt/i18n.ts
+
+    const sourcePath = resolve(wxt.config.wxtDir, 'i18n/index.ts');
+    const typesPath = resolve(wxt.config.wxtDir, 'i18n/structure.d.ts');
+
+    wxt.hooks.hook('prepare:types', async (_, entries) => {
       entries.push({
         path: sourcePath,
         text: `import { createI18n } from '@wxt-dev/i18n';
+import type { GeneratedI18nStructure } from './structure';
 
-${generateTypeText(messages)}
 export const i18n = createI18n<GeneratedI18nStructure>();
+
+export { GeneratedI18nStructure }
 `,
       });
     });
 
     addAlias(wxt, '#i18n', sourcePath);
 
+    // Generate separate declaration file containing types - this prevents
+    // firing the dev server's default file watcher when updating the types,
+    // which would cause a full rebuild and reload of the extension.
+
+    wxt.hooks.hook('prepare:types', async (_, entries) => {
+      entries.push(await generateTypes());
+    });
+
     // Generate _locales/.../messages.json files
 
     wxt.hooks.hook('build:publicAssets', async (_, assets) => {
-      const files = await getLocalizationFiles();
-      await Promise.all(
-        files.map(async ({ file, locale }) => {
-          const messages = await parseMessagesFile(file);
-          assets.push({
-            contents: generateChromeMessagesText(messages),
-            relativeDest: join('_locales', locale, 'messages.json'),
-          });
-        }),
-      );
+      const outFiles = await generateOutputJsonFiles();
+      assets.push(...outFiles);
+    });
+
+    // Reload extension during development
+
+    wxt.hooks.hookOnce('build:done', () => {
+      const watcher = watch(resolve(wxt.config.srcDir, 'locales'));
+      watcher.on('change', updateLocalizations);
     });
   },
 });
 
-export interface I18nModuleOptions {
-  clientId: string;
-}
+// export interface I18nModuleOptions {}
 
-declare module 'wxt' {
-  export interface InlineConfig {
-    i18n?: I18nModuleOptions;
-  }
-}
+// declare module 'wxt' {
+//   export interface InlineConfig {
+//     i18n?: I18nModuleOptions;
+//   }
+// }
