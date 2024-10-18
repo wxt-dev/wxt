d38100be3053ea726c119a8bcf37de07229a7557
Craig Slusher <cslusher@obsidiansecurity.com>
2024-10-18 13:24:25 -0400

    Adds browser to config resolution lookup path

--- packages/wxt/src/core/utils/env.ts
+++ packages/wxt/src/core/utils/env.ts
@@ -1,10 +1,18 @@
 import { config } from 'dotenv';
+import type { TargetBrowser } from '../../types';
 
 /**
- * Load environment files based on the current mode.
+ * Load environment files based on the current mode and browser.
  */
-export function loadEnv(mode: string) {
+export function loadEnv(mode: string, browser: TargetBrowser) {
   return config({
-    path: [`.env.${mode}.local`, `.env.${mode}`, `.env.local`, `.env`],
+    path: [
+      `.env.${mode}.${browser}.local`,
+      `.env.${mode}.${browser}`,
+      `.env.${mode}.local`,
+      `.env.${mode}`,
+      `.env.local`,
+      `.env`,
+    ],
   });
 }
