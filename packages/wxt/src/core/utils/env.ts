564da4aff396c0d3e32166ab24a1f88a6b2d42dd
Aaron <aaronklinker1@gmail.com>
2024-10-19 11:15:02 -0500

    Add other variants

--- packages/wxt/src/core/utils/env.ts
+++ packages/wxt/src/core/utils/env.ts
@@ -6,9 +6,12 @@ import type { TargetBrowser } from '../../types';
  */
 export function loadEnv(mode: string, browser: TargetBrowser) {
   return config({
+    // Files on top override files below
     path: [
       `.env.${mode}.${browser}.local`,
       `.env.${mode}.${browser}`,
+      `.env.${browser}.local`,
+      `.env.${browser}`,
       `.env.${mode}.local`,
       `.env.${mode}`,
       `.env.local`,
