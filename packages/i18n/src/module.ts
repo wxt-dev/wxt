22b529453f4867b6b5687ec44f23e0a4c93ba3a5
Aaron <aaronklinker1@gmail.com>
2024-10-19 00:43:30 -0500

    fix: Add missing "type" keyword to type export in generated file

--- packages/i18n/src/module.ts
+++ packages/i18n/src/module.ts
@@ -128,7 +128,7 @@ import type { GeneratedI18nStructure } from './structure';
 
 export const i18n = createI18n<GeneratedI18nStructure>();
 
-export { GeneratedI18nStructure }
+export { type GeneratedI18nStructure }
 `,
       });
     });
