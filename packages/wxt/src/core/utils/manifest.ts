55f19b8088076df4efd59833c53e54e9ec3c740a
Aaron <aaronklinker1@gmail.com>
2024-11-16 11:38:21 -0600

    fix!: Remove `transformManfiest` option

--- packages/wxt/src/core/utils/manifest.ts
+++ packages/wxt/src/core/utils/manifest.ts
@@ -107,8 +107,6 @@ export async function generateManifest(
   if (wxt.config.command === 'serve') addDevModeCsp(manifest);
   if (wxt.config.command === 'serve') addDevModePermissions(manifest);
 
-  // TODO: Remove in v1
-  wxt.config.transformManifest?.(manifest);
   await wxt.hooks.callHook('build:manifestGenerated', wxt, manifest);
 
   if (wxt.config.manifestVersion === 2) {
