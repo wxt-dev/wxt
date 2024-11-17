0d0728a9eef238480785b5794d45c517a92d6db1
Aaron <aaronklinker1@gmail.com>
2024-11-17 14:51:54 -0600

    Fix type error

--- templates/svelte/src/entrypoints/popup/main.ts
+++ templates/svelte/src/entrypoints/popup/main.ts
@@ -3,7 +3,7 @@ import App from './App.svelte';
 import './app.css';
 
 const app = mount(App, {
-  target: document.getElementById('app'),
+  target: document.getElementById('app')!,
 });
 
 export default app;
