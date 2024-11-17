b44bfe920976b0492b4cfeaee13b5af5712a490c
Aaron <aaronklinker1@gmail.com>
2024-11-17 14:46:58 -0600

    Fix formatting

--- templates/svelte/src/entrypoints/popup/main.ts
+++ templates/svelte/src/entrypoints/popup/main.ts
@@ -1,11 +1,9 @@
-import { mount } from 'svelte'
+import { mount } from 'svelte';
 import App from './App.svelte';
 import './app.css';
 
-const app = mount(App,
-  {
-    target: document.getElementById("app")
-  }
-);
+const app = mount(App, {
+  target: document.getElementById('app'),
+});
 
 export default app;
