e93d461458f137f8eba5e3f7b0700854f6174274
PatatoBit <pathonscript@gmail.com>
2024-11-17 22:12:50 +0700

    Updated to Svelte 5

--- templates/svelte/src/entrypoints/popup/main.ts
+++ templates/svelte/src/entrypoints/popup/main.ts
@@ -1,8 +1,11 @@
-import './app.css';
+import { mount } from 'svelte'
 import App from './App.svelte';
+import './app.css';
 
-const app = new App({
-  target: document.getElementById('app')!,
-});
+const app = mount(App,
+  {
+    target: document.getElementById("app")
+  }
+);
 
 export default app;
