4fb0342be827086db6bda6ccbf68f50d860e9536
Aaron <aaronklinker1@gmail.com>
2024-11-27 23:11:16 -0600

    cleanup

--- packages/wxt/src/core/utils/eslint.ts
+++ packages/wxt/src/core/utils/eslint.ts
@@ -1,8 +1,8 @@
-const eslintPackageName = 'eslint';
+const ESLINT_PACKAGE_NAME = 'eslint';
 
 export async function getEslintVersion(): Promise<string[]> {
   try {
-    const { version } = await import(eslintPackageName);
+    const { version } = await import(ESLINT_PACKAGE_NAME);
     return version.split('.') ?? [];
   } catch {
     // Return an empty version when there's an error importing ESLint
