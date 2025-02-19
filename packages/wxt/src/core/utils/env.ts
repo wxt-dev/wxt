a38e3c27330ab82f9c136191fc85b9b101516915
Stefan Maric <me@stefanmaric.com>
2025-02-19 04:47:47 +0100

    fix: regression on variable expansion in dotenv files (#1449)

--- packages/wxt/src/core/utils/env.ts
+++ packages/wxt/src/core/utils/env.ts
@@ -1,21 +1,24 @@
 import { config } from 'dotenv';
+import { expand } from 'dotenv-expand';
 import type { TargetBrowser } from '../../types';
 
 /**
  * Load environment files based on the current mode and browser.
  */
 export function loadEnv(mode: string, browser: TargetBrowser) {
-  return config({
-    // Files on top override files below
-    path: [
-      `.env.${mode}.${browser}.local`,
-      `.env.${mode}.${browser}`,
-      `.env.${browser}.local`,
-      `.env.${browser}`,
-      `.env.${mode}.local`,
-      `.env.${mode}`,
-      `.env.local`,
-      `.env`,
-    ],
-  });
+  return expand(
+    config({
+      // Files on top override files below
+      path: [
+        `.env.${mode}.${browser}.local`,
+        `.env.${mode}.${browser}`,
+        `.env.${browser}.local`,
+        `.env.${browser}`,
+        `.env.${mode}.local`,
+        `.env.${mode}`,
+        `.env.local`,
+        `.env`,
+      ],
+    }),
+  );
 }
