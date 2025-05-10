8c944cf54791bc5f8dbca593915d93bcbf2087bd
Qiwei Yang <yangqiwei97@gmail.com>
2025-05-10 09:09:12 +0800

    docs: Added "Bilibili Feed History Extension" to the homepage

--- docs/.vitepress/components/UsingWxtSection.vue
+++ docs/.vitepress/components/UsingWxtSection.vue
@@ -77,6 +77,7 @@ const chromeExtensionIds = [
   'pamnlaoeobcmhkliljfaofekeddpmfoh', // Web to PDF
   'jmbcbeepjfenihlocplnbmbhimcoooka', // Online CSV Viewer
   'nkjcoophmpcmmgadnljnlpbpfdfacgbo', // YouTube Video Transcript
+  'npfopljnjbamegincfjelhjhnonnjloo', // Bilibili Feed History Helper
 ];
 
 const { data, err, isLoading } = useListExtensionDetails(chromeExtensionIds);
