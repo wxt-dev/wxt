ad4de4b92c7a1081b9de734672d1dc27d49c6dcc
Stepan Rabotkin <epicstyt@gmail.com>
2025-05-14 13:23:48 +0300

    docs: Added CanCopy to homepage (#1667)
    Co-authored-by: Florian Metz <me@timeraa.dev>

--- docs/.vitepress/components/UsingWxtSection.vue
+++ docs/.vitepress/components/UsingWxtSection.vue
@@ -77,6 +77,7 @@ const chromeExtensionIds = [
   'pamnlaoeobcmhkliljfaofekeddpmfoh', // Web to PDF
   'jmbcbeepjfenihlocplnbmbhimcoooka', // Online CSV Viewer
   'nkjcoophmpcmmgadnljnlpbpfdfacgbo', // YouTube Video Transcript
+  'ggcfemmoabhhelfkhknhbnkmeahloiod', // CanCopy - A web extension that allow you to copy any content from website
   'modkelfkcfjpgbfmnbnllalkiogfofhb', // Language Learning with AI
   'npfopljnjbamegincfjelhjhnonnjloo', // Bilibili Feed History Helper
   'edkhpdceeinkcacjdgebjehipmnbomce', // NZBDonkey - The ultimate NZB file download tool
