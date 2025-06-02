28ef6c437d4b2ec13401fe0287e93d5f3c0ac435
Aaron <aaronklinker1@gmail.com>
2025-06-02 17:26:04 -0500

    throw MV3 dev error regardless of runner

--- packages/wxt/src/core/runners/index.ts
+++ packages/wxt/src/core/runners/index.ts
@@ -9,6 +9,12 @@ import { wxt } from '../wxt';
 export async function createExtensionRunner(): Promise<ExtensionRunner> {
   if (wxt.config.browser === 'safari') return createSafariRunner();
 
+  if (wxt.config.browser === 'firefox' && wxt.config.manifestVersion === 3) {
+    throw Error(
+      'Dev mode does not support Firefox MV3. For alternatives, see https://github.com/wxt-dev/wxt/issues/230#issuecomment-1806881653',
+    );
+  }
+
   if (await isWsl()) return createWslRunner();
   if (wxt.config.runnerConfig.config?.disabled) return createManualRunner();
 
