4923ec6d5bc5f0219f544e6bc96712dc9522ffeb
Yacine Benslimane <82149396+yacine-bens@users.noreply.github.com>
2025-06-11 08:49:36 +0100

    Update UsingWxtSection.vue

--- docs/.vitepress/components/UsingWxtSection.vue
+++ docs/.vitepress/components/UsingWxtSection.vue
@@ -86,6 +86,7 @@ const chromeExtensionIds = [
   'cckggnbnimdbbpmdinkkgbbncopbloob', // WeChat Markdown Editor(微信 Markdown 编辑器)
   'jcblcjolcojmfopefcighfmkkefbaofg', // Tab Grab
   'eehmoikadcijkapfjocnhjclpbaindlb', // BrowserLens - https://browserlens.com/
+  'hfhellofkjebbchcdffmicekjdomkcmc', // Epic Games Library Extension
 ];
 
 const { data, err, isLoading } = useListExtensionDetails(chromeExtensionIds);
