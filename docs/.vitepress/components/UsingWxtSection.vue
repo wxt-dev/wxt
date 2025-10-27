a2d6577579c90acd223e491eceb255e48c878bf5
Aaron <aaronklinker1@gmail.com>
2025-10-27 17:20:59 -0500

    docs: Fix syntax error

--- docs/.vitepress/components/UsingWxtSection.vue
+++ docs/.vitepress/components/UsingWxtSection.vue
@@ -102,7 +102,7 @@ const chromeExtensionIds = [
   'ehmoihnjgkdimihkhokkmfjdgomohjgm', // Filmbudd Pro - Simple, private – and synced ratings and watch notes across all your devices
   'alglchohmdikgdjhafiicilegegieafa', // MultiField CopyCat - Copy, Paste & Autofill Web Forms Instantly
   'aamihahiiogceidpbnfgehacgiecephe', // ChatSight - Add Table of Contents to ChatGPT
-  'cndibmoanboadcifjkjbdpjgfedanolh'  // BetterCampus (prev. BetterCanvas)
+  'cndibmoanboadcifjkjbdpjgfedanolh', // BetterCampus (prev. BetterCanvas)
   'hinfimgacobnellbncbcpdlpaapcofaa', // Leetcode Fonts - Change fonts in leetcode effortlessly
   'kbkbfefhhabpkibojinapkkgciiacggg', // TranslateManga - Manga Translator & Manga Tracker
   'emeakbgdecgmdjgegnejpppcnkcnoaen', // SiteData - Free Website Traffic Checker & Reverse AdSense Tool
