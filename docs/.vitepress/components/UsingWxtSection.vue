bc79987ed5b98020b42835c8145e748344712abc
yukidaruma <6897821+yukidaruma@users.noreply.github.com>
2025-10-28 07:04:46 +0900

    docs: Added "Livestream Chat Reader" to the homepage (#1910)
    Co-authored-by: Aaron <aaronklinker1@gmail.com>

--- docs/.vitepress/components/UsingWxtSection.vue
+++ docs/.vitepress/components/UsingWxtSection.vue
@@ -102,6 +102,7 @@ const chromeExtensionIds = [
   'ehmoihnjgkdimihkhokkmfjdgomohjgm', // Filmbudd Pro - Simple, private – and synced ratings and watch notes across all your devices
   'alglchohmdikgdjhafiicilegegieafa', // MultiField CopyCat - Copy, Paste & Autofill Web Forms Instantly
   'aamihahiiogceidpbnfgehacgiecephe', // ChatSight - Add Table of Contents to ChatGPT
+  'gpnckbhgpnjciklpoehkmligeaebigaa', // Livestream Chat Reader - Text-to-Speech for YouTube/Twitch chat
   'fjlalaedpfcojcfpkgkglbjjbbkofgnl', // ChatGPT Token Counter - Count tokens in real time on chatgpt conversation
   'fbgblmjbeebanackldpbmpacppflgmlj', // LinuxDo Scripts - 为 linux.do 用户提供了一些增强功能
 ];
