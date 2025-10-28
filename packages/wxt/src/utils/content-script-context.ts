3c745a90dbeaac36859abe16ddf2dba1a6656ec2
Namu <535248+namukang@users.noreply.github.com>
2025-10-28 10:49:10 +0800

    feat: invalidate using window.postMessage for backwards compatibility

--- packages/wxt/src/utils/content-script-context.ts
+++ packages/wxt/src/utils/content-script-context.ts
@@ -247,6 +247,17 @@ export class ContentScriptContext implements AbortController {
         },
       }),
     );
+
+    // Send message using `window.postMessage` for backwards compatibility to invalidate old versions before WXT changed to `document.dispatchEvent`
+    // TODO: Remove this once WXT version using `document.dispatchEvent` has been released for a while
+    window.postMessage(
+      {
+        type: ContentScriptContext.SCRIPT_STARTED_MESSAGE_TYPE,
+        contentScriptName: this.contentScriptName,
+        messageId: this.id,
+      },
+      '*',
+    );
   }
 
   verifyScriptStartedEvent(event: CustomEvent) {
