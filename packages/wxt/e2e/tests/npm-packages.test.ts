70c3ba7c7ee66ab523cff3c3ae2c6724f3f3c657
Aaron <aaronklinker1@gmail.com>
2025-12-18 17:23:25 -0600

    Fix test

--- packages/wxt/e2e/tests/npm-packages.test.ts
+++ packages/wxt/e2e/tests/npm-packages.test.ts
@@ -9,7 +9,7 @@ test('Only one version of esbuild should be installed (each version is ~20mb of 
 
   // This text represents the wxt package being responsible for a version of esbuild?
   // If this doesn't work, we'll need to find a better way to check this.
-  const count = stdout.match(/\n    └─ wxt@workspace/g)?.length || 0;
+  const count = stdout.match(/\n  [└├]─ wxt@workspace/g)?.length || 0;
 
   expect(count).toBe(1);
 });
