0ceedb0de90649d533821af90f1df4c6f4e64ce7
Aaron <aaronklinker1@gmail.com>
2025-12-18 17:21:19 -0600

    Fix test

--- packages/wxt/e2e/tests/npm-packages.test.ts
+++ packages/wxt/e2e/tests/npm-packages.test.ts
@@ -1,35 +1,15 @@
 import { test, expect } from 'vitest';
 import spawn from 'nano-spawn';
-import {
-  NpmListDependency,
-  NpmListProject,
-} from '../../src/core/package-managers/npm';
 
 // Tests to ensure the total size of the WXT module is as small as possible
 // https://pkg-size.dev/wxt
 
 test('Only one version of esbuild should be installed (each version is ~20mb of node_modules)', async () => {
-  const { stdout } = await spawn('bun', ['why', 'esbuild', '--prod', '--json']);
-  const projects: NpmListProject[] = JSON.parse(stdout);
-  const esbuildVersions = new Set<string>();
-  iterateDependencies(projects, (name, meta) => {
-    if (name === 'esbuild') esbuildVersions.add(meta.version);
-  });
+  const { stdout } = await spawn('bun', ['why', 'esbuild']);
 
-  expect([...esbuildVersions]).toHaveLength(1);
+  // This text represents the wxt package being responsible for a version of esbuild?
+  // If this doesn't work, we'll need to find a better way to check this.
+  const count = stdout.match(/\n    └─ wxt@workspace/g)?.length || 0;
+
+  expect(count).toBe(1);
 });
-
-function iterateDependencies(
-  projects: NpmListProject[],
-  cb: (name: string, meta: NpmListDependency) => void,
-) {
-  const recurse = (dependencies: Record<string, NpmListDependency>) => {
-    Object.entries(dependencies).forEach(([name, meta]) => {
-      cb(name, meta);
-      if (meta.dependencies) recurse(meta.dependencies);
-    });
-  };
-  projects.forEach((project) => {
-    if (project.dependencies) recurse(project.dependencies);
-  });
-}
