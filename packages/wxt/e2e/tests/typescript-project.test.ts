b263898e0e25a9cf1ad5a13cf0d943b1cb970cea
PatrykKuniczak <p.kuniczak@gmail.com>
2025-12-25 16:46:37 +0100

    fix(packages/wxt): change HtmlPublicPath from `type` to `const` for fix `Unresolved variable or type HtmlPublicPath` error

--- packages/wxt/e2e/tests/typescript-project.test.ts
+++ packages/wxt/e2e/tests/typescript-project.test.ts
@@ -53,7 +53,7 @@ describe('TypeScript Project', () => {
           | "/options.html"
           | "/popup.html"
           | "/sandbox.html"
-        type HtmlPublicPath = Extract<PublicPath, \`\${string}.html\`>
+        const HtmlPublicPath = Extract<PublicPath, \`\${string}.html\`>
         export interface WxtRuntime {
           getURL(path: PublicPath): string;
           getURL(path: \`\${HtmlPublicPath}\${string}\`): string;
