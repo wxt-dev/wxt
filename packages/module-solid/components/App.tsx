32c858f4d95a1dbacaa50708a58989d6bf73cf07
PatrykKuniczak <p.kuniczak@gmail.com>
2025-12-28 13:14:49 +0100

    clean(packages/module-solid): improve code readability

--- packages/module-solid/components/App.tsx
+++ packages/module-solid/components/App.tsx
@@ -2,6 +2,8 @@ import { Component } from 'solid-js';
 
 export const App: Component = () => {
   const [count, setCount] = createSignal(0);
+
   const increment = () => setCount((count) => count + 1);
+
   return <button onClick={increment}>Count: {count()}</button>;
 };
