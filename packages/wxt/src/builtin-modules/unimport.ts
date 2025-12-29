a59cf908fcbcd68b9a14b852e86b6d6c0451a490
PatrykKuniczak <p.kuniczak@gmail.com>
2025-12-29 19:21:36 +0100

    clean(packages/wxt/builtin-modules): make code more readable

--- packages/wxt/src/builtin-modules/unimport.ts
+++ packages/wxt/src/builtin-modules/unimport.ts
@@ -13,6 +13,7 @@ export default defineWxtModule({
   name: 'wxt:built-in:unimport',
   setup(wxt) {
     let unimport: Unimport;
+
     const isEnabled = () => !wxt.config.imports.disabled;
 
     // Add user module imports to config
@@ -91,6 +92,7 @@ async function getImportsModuleEntry(
   unimport: Unimport,
 ): Promise<WxtDirFileEntry> {
   const imports = await unimport.getImports();
+
   return {
     path: 'types/imports-module.d.ts',
     text: [
@@ -119,8 +121,11 @@ async function getEslintConfigEntry(
       return globals;
     }, {});
 
-  if (version <= 8) return getEslint8ConfigEntry(options, globals);
-  else return getEslint9ConfigEntry(options, globals);
+  if (version <= 8) {
+    return getEslint8ConfigEntry(options, globals);
+  } else {
+    return getEslint9ConfigEntry(options, globals);
+  }
 }
 
 export function getEslint8ConfigEntry(
