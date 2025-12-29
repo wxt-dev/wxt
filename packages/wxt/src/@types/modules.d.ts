3a319bb8e051db05f9c87457f05113affa0764d8
PatrykKuniczak <p.kuniczak@gmail.com>
2025-12-29 19:19:16 +0100

    rename `zipdir` to `zipDir`

--- packages/wxt/src/@types/modules.d.ts
+++ packages/wxt/src/@types/modules.d.ts
@@ -1,20 +1,20 @@
 // Custom TS definitions for non-TS packages
 
 declare module 'zip-dir' {
-  // Represents the options object for zipdir function
+  // Represents the options object for zipDir function
   interface ZipDirOptions {
     saveTo?: string;
     filter?: (path: string, stat: import('fs').Stats) => boolean;
     each?: (path: string) => void;
   }
 
-  function zipdir(
+  function zipDir(
     dirPath: string,
     options?: ZipDirOptions,
     callback?: (error: Error | null, buffer: Buffer) => void,
   ): Promise<Buffer>;
 
-  export = zipdir;
+  export = zipDir;
 }
 
 declare module 'web-ext-run' {
