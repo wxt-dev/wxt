ee1e87be73db443fa9549d8a2a3f04cfe5ac3902
Scan <103391616+scanash00@users.noreply.github.com>
2026-02-01 04:53:21 -0900

    Add Margin extension to extensions using wxt

--- docs/.vitepress/components/UsingWxtSection.vue
+++ docs/.vitepress/components/UsingWxtSection.vue
@@ -112,6 +112,7 @@ const chromeExtensionIds = [
   'dfacnjidgbagicaekenjgclfnhdnjjdi', // Zen Virtual Piano - https://zen-piano.web.app/
   'naeibcegmgpofimedkmfgjgphfhfhlab', // Crypto Pulse price tracker - https://get-crypto-pulse.web.app/
   'ffglckbhfbfmdkefdmjbhpnffkcmlhdh', // Redirect Web - Automatically redirect pages or open them in another app
+  'cgpmbiiagnehkikhcbnhiagfomajncpa', // Margin - Annotate and highlight any webpage, with your notes saved to the decentralized AT Protocol.
 ];
 
 const { data, err, isLoading } = useListExtensionDetails(chromeExtensionIds);
