90083381c45933a6b4d4496e3cfbdf190dfac714
Aaron <aaronklinker1@gmail.com>
2026-02-08 12:07:50 -0600

    chore: Cleanup comment line lengths

--- packages/wxt/src/core/utils/cli.ts
+++ packages/wxt/src/core/utils/cli.ts
@@ -9,8 +9,8 @@ export function defineCommand<TArgs extends any[]>(
   },
 ) {
   return async (...args: TArgs) => {
-    // Enable consola's debug mode globally at the start of all commands when the `--debug` flag is
-    // passed
+    // Enable consola's debug mode globally at the start of all commands when
+    // the `--debug` flag is passed
     const isDebug = !!args.find((arg) => arg?.debug);
     if (isDebug) {
       consola.level = LogLevels.debug;
