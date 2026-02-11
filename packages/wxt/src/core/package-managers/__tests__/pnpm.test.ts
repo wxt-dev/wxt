d2ef0afcd451728eae2ed84e8a7b6e72997c26c6
Aaron <aaronklinker1@gmail.com>
2026-02-11 16:01:32 -0600

    cleanup

--- packages/wxt/src/core/package-managers/__tests__/pnpm.test.ts
+++ packages/wxt/src/core/package-managers/__tests__/pnpm.test.ts
@@ -10,7 +10,7 @@ describe('PNPM Package Management Utils', () => {
     const cwd = path.resolve(__dirname, 'fixtures/simple-pnpm-project');
     beforeAll(async () => {
       // PNPM needs the modules installed, or 'pnpm ls' will return a blank list.
-      await spawn('pnpm', ['install'], { cwd, stdio: 'inherit' });
+      await spawn('pnpm', ['install'], { cwd });
     });
 
     it('should list direct dependencies', async () => {
