0af09ca302f582da2db696932beb134b0c5c9733
Aaron <aaronklinker1@gmail.com>
2026-02-11 17:02:42 -0600

    Try with --bun?

--- packages/wxt/e2e/tests/init.test.ts
+++ packages/wxt/e2e/tests/init.test.ts
@@ -1,32 +1,19 @@
 import glob from 'fast-glob';
 import { mkdir, writeJson } from 'fs-extra';
-import spawn from 'nano-spawn';
 import { describe, expect, it } from 'vitest';
-import { TestProject, WXT_PACKAGE_DIR } from '../utils';
+import { initialize } from '../../src';
+import { TestProject } from '../utils';
 
 describe('Init command', () => {
   it('should download and create a template', async () => {
     const project = new TestProject();
 
-    await spawn(
-      'bun',
-      [
-        'run',
-        '--silent',
-        'wxt',
-        'init',
-        project.root,
-        '-t',
-        'vue',
-        '--pm',
-        'npm',
-      ],
-      {
-        env: { CI: 'true' },
-        stdio: 'inherit',
-        cwd: WXT_PACKAGE_DIR,
-      },
-    );
+    await initialize({
+      directory: project.root,
+      packageManager: 'npm',
+      template: 'vue',
+    });
+
     const files = await glob('**/*', {
       cwd: project.root,
       onlyFiles: true,
@@ -65,25 +52,11 @@ describe('Init command', () => {
     await writeJson(project.resolvePath('package.json'), {});
 
     await expect(() =>
-      spawn(
-        'bun',
-        [
-          'run',
-          '--silent',
-          'wxt',
-          'init',
-          project.root,
-          '-t',
-          'vue',
-          '--pm',
-          'npm',
-        ],
-        {
-          env: { CI: 'true' },
-          stdio: 'ignore',
-          cwd: WXT_PACKAGE_DIR,
-        },
-      ),
+      initialize({
+        directory: project.root,
+        template: 'vue',
+        packageManager: 'npm',
+      }),
     ).rejects.toThrowError('Command failed with exit code 1:');
   });
 });
