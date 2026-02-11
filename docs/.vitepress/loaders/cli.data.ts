e34865c37f7edbe526e0a06d72a9bac6e91fbe85
Aaron <aaronklinker1@gmail.com>
2026-02-11 17:26:06 -0600

    debug netlify

--- docs/.vitepress/loaders/cli.data.ts
+++ docs/.vitepress/loaders/cli.data.ts
@@ -1,5 +1,6 @@
-import { resolve } from 'node:path';
 import consola from 'consola';
+import fs from 'fs-extra';
+import { resolve } from 'node:path';
 
 const cliDir = resolve('packages/wxt/src/cli/commands');
 const cliDirGlob = resolve(cliDir, '**');
@@ -53,6 +54,8 @@ function getWxtHelp(command: string): Promise<string> {
 }
 
 async function getPublishExtensionHelp(command: string): Promise<string> {
+  console.log('CWD:', process.cwd());
+  console.log(await fs.readdir('./packages/wxt/node_modules/.bin'));
   const res = await getHelp(
     `./packages/wxt/node_modules/.bin/publish-extension ${command}`.trim(),
   );
