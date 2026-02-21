1b546c826eb676c1e8420de5ee74d2711111e258
Aaron <aaronklinker1@gmail.com>
2026-02-21 09:34:39 -0600

    Add JSDoc to app config APIs

--- packages/wxt/src/utils/app-config.ts
+++ packages/wxt/src/utils/app-config.ts
@@ -3,10 +3,20 @@
 import appConfig from 'virtual:app-config';
 import type { WxtAppConfig } from './define-app-config';
 
+/**
+ * Get runtime config defined in `<srcDir>/app.config.ts`
+ *
+ * @see https://wxt.dev/guide/essentials/config/runtime.html
+ */
 export function getAppConfig(): WxtAppConfig {
   return appConfig;
 }
 
+/**
+ * Alias for {@link getAppConfig}.
+ *
+ * @see https://wxt.dev/guide/essentials/config/runtime.html
+ */
 export function useAppConfig(): WxtAppConfig {
   return getAppConfig();
 }
