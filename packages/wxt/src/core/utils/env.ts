90cb1d24ad6d704b9e7b48e6322f89d4f444550c
Aaron <aaronklinker1@gmail.com>
2026-03-18 10:01:37 -0500

    simplify

--- packages/wxt/src/core/utils/env.ts
+++ packages/wxt/src/core/utils/env.ts
@@ -6,36 +6,36 @@ import type { TargetBrowser } from '../../types';
 /** Load environment files based on the current mode and browser. */
 export function loadEnv(mode: string, browser: TargetBrowser) {
   const envFiles = [
-    `.env.${mode}.${browser}.local`,
-    `.env.${mode}.${browser}`,
-    `.env.${browser}.local`,
-    `.env.${browser}`,
-    `.env.${mode}.local`,
-    `.env.${mode}`,
-    `.env.local`,
+    // List is ordered with general files first, specific ones last, so the more
+    // specific files override the more general ones in the loop below.
     `.env`,
+    `.env.local`,
+    `.env.${mode}`,
+    `.env.${mode}.local`,
+    `.env.${browser}`,
+    `.env.${browser}.local`,
+    `.env.${mode}.${browser}`,
+    `.env.${mode}.${browser}.local`,
   ];
 
-  // We reversed the list so that more specific files take priority over more general ones.
-  const parsed = Object.fromEntries(
-    envFiles
-      .slice()
-      .reverse()
-      .flatMap((filePath) => {
-        if (!existsSync(filePath)) return [];
+  const parsed = Object.fromEntries<string>(
+    envFiles.flatMap((filePath) => {
+      if (!existsSync(filePath)) return [];
 
-        try {
-          const content = readFileSync(filePath, 'utf-8');
-          const parsedEnv = parseEnv(content);
-          return Object.entries(parsedEnv);
-        } catch {
-          return [];
-        }
-      }),
-  ) as Record<string, string>;
+      try {
+        const content = readFileSync(filePath, 'utf-8');
+        const parsedEnv = parseEnv(content);
+        return Object.entries(parsedEnv) as [string, string][];
+      } catch {
+        return [];
+      }
+    }),
+  );
 
-  // We prepare the environment for variable expansion ($VAR)
+  // Make a copy of `process.env` so that `dotenv-expand` doesn't re-assign the
+  // expanded values to the global `process.env`.
   const processEnv = { ...process.env } as Record<string, string>;
+
   expand({
     parsed,
     processEnv,
