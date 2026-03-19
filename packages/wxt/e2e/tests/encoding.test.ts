4a6d515c32b6cacf2014235322b82ad91b2e79cc
Aaron <aaronklinker1@gmail.com>
2026-03-19 16:16:16 -0500

    Update test

--- packages/wxt/e2e/tests/encoding.test.ts
+++ packages/wxt/e2e/tests/encoding.test.ts
@@ -4,20 +4,20 @@ import { TestProject } from '../utils';
 
 describe('Encoding', () => {
   it('should convert unicode characters to ascii escaped chars', async () => {
-    const UTF8_CHAR = '丂';
-    const ASCII_ESCAPED = '\\u4E02';
+    const KNOWN_BAD_CHAR = '￿'; // This is \uFFFF
+    const ESCAPED_BAD_CHAR = '\\uFFFF';
 
     const project = new TestProject();
 
     // `project.addFile` writes the file as UTF8
     project.addFile(
       'entrypoints/example.ts',
-      `export default defineUnlistedScript(() => console.log('${UTF8_CHAR}'))`,
+      `export default defineUnlistedScript(() => console.log('${KNOWN_BAD_CHAR}'))`,
     );
     await project.build();
 
     const file = project.resolvePath('.output/chrome-mv3/example.js');
     const asciiOutput = await readFile(file, 'ascii');
-    expect(asciiOutput).toContain(ASCII_ESCAPED);
+    expect(asciiOutput).toContain(ESCAPED_BAD_CHAR);
   });
 });
