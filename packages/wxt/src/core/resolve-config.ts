225a94199ce0ab33d2c3bc99eb0bf3310885bf5d
Suvesh Moza <anmolmoza2@gmail.com>
2026-04-13 19:04:46 +0530

    feat: add support for Firefox data collection permissions (#1976)
    Co-authored-by: Patryk Kuniczak <p.kuniczak@gmail.com>
    Co-authored-by: Aaron <aaronklinker1@gmail.com>
    Co-authored-by: Nick Doan <nickbar01234@gmail.com>
    Co-authored-by: Tam Dang <139360620+dahomita@users.noreply.github.com>

--- packages/wxt/src/core/resolve-config.ts
+++ packages/wxt/src/core/resolve-config.ts
@@ -227,6 +227,7 @@ export async function resolveConfig(
     userConfigMetadata: userConfigMetadata ?? {},
     alias,
     experimental: defu(mergedConfig.experimental, {}),
+    suppressWarnings: mergedConfig.suppressWarnings ?? {},
     dev: {
       server: devServerConfig,
       reloadCommand,
