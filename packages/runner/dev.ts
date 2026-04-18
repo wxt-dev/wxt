c4636b83598dddcef6135200f197723389a828b6
Aaron <aaronklinker1@gmail.com>
2026-04-18 10:00:22 -0500

    Cleanup debug logs

--- packages/runner/dev.ts
+++ packages/runner/dev.ts
@@ -7,6 +7,9 @@
 
 import { run } from './src';
 
+// Uncomment to enable debug logs
+process.env.DEBUG = 'wxt:runner:*';
+
 await run({
   extensionDir: 'demo-extension-1',
   chromiumAdditionalExtensionDirs: ['demo-extension-2'],
