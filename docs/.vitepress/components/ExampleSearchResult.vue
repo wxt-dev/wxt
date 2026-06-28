bde65e20663d4c34356137a798d0a9f0d374ea2e
Patryk Kuniczak <p.kuniczak@gmail.com>
2026-06-28 20:53:25 +0200

    docs: Make vitepress components css files more readable and simply (#2272)

--- docs/.vitepress/components/ExampleSearchResult.vue
+++ docs/.vitepress/components/ExampleSearchResult.vue
@@ -17,56 +17,45 @@ defineProps<{
 </template>
 
 <style scoped>
-* {
-  min-width: 0;
-}
 a {
   padding: 16px;
   display: flex;
   flex-direction: column;
   border: 2px solid var(--vp-c-bg-soft);
   border-radius: 16px;
-  color: var(--vp-c-text-1) !important;
   gap: 8px;
-}
 
-a:hover {
-  outline: 2px solid var(--vp-c-brand-2);
-}
-.name {
-  font-size: 16px;
-  font-weight: 500;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  flex-shrink: 0;
+  &:hover {
+    outline: 2px solid var(--vp-c-brand-3);
 
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-}
-.description {
-  height: 53px;
-  opacity: 70%;
-  font-size: 14px;
-  font-weight: normal;
-  line-height: 120%;
-  min-height: 0;
+    .link {
+      opacity: 100%;
+    }
+  }
 
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-}
+  .name {
+    font-weight: 500;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
 
-.link {
-  opacity: 0;
-  transition: 250ms;
-  color: var(--vp-c-brand-2);
-  font-weight: bold;
-  text-align: right;
-}
-a:hover .link {
-  opacity: 100%;
+  .description {
+    opacity: 70%;
+    font-size: 14px;
+    line-height: 130%;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+
+  .link {
+    opacity: 0;
+    transition: 250ms;
+    color: var(--vp-c-brand-3);
+    font-weight: bold;
+    text-align: right;
+  }
 }
 </style>
