c5a25c4bfd52663384f72091e75c9067c7900002
nishu-murmu <nishumurmu017@gmail.com>
2026-07-01 20:54:29 +0530

    chore: using `wxt.logger` instead of consola.

--- packages/wxt/src/core/builders/vite/plugins/download.ts
+++ packages/wxt/src/core/builders/vite/plugins/download.ts
@@ -1,7 +1,7 @@
+import { wxt } from '../../../wxt';
 import type { Plugin } from 'vite';
 import type { ResolvedConfig } from '../../../../types';
 import { fetchCached } from '../../../utils/network';
-import consola from 'consola';
 
 /**
  * Downloads any URL imports, like Google Analytics, into virtual modules so
@@ -32,7 +32,7 @@ export function download(config: ResolvedConfig): Plugin {
       },
       handler(id) {
         const url = id.replace('\0url:', '');
-        consola.warn(
+        wxt.logger.warn(
           '`url:` import is deprecated, see https://github.com/wxt-dev/wxt/issues/2262. You can download the files and host it in your repo instead',
         );
         return fetchCached(url, config);
