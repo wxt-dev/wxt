46cd54da3fe4684c6b9caf4c2f4f3c374f281689
nishu-murmu <nishumurmu017@gmail.com>
2026-07-01 12:13:22 +0530

    fix: showing warning while using the url import feature and updating docs.

--- packages/wxt/src/core/builders/vite/plugins/download.ts
+++ packages/wxt/src/core/builders/vite/plugins/download.ts
@@ -1,6 +1,7 @@
 import type { Plugin } from 'vite';
 import type { ResolvedConfig } from '../../../../types';
 import { fetchCached } from '../../../utils/network';
+import consola from 'consola';
 
 /**
  * Downloads any URL imports, like Google Analytics, into virtual modules so
@@ -31,6 +32,9 @@ export function download(config: ResolvedConfig): Plugin {
       },
       handler(id) {
         const url = id.replace('\0url:', '');
+        consola.warn(
+          'The `url:` import feature is deprecated. See https://github.com/wxt-dev/wxt/issues/2262.',
+        );
         return fetchCached(url, config);
       },
     },
