9698f2fdd9832da2a452fb7557cf1ed4fc882f1f
fkatsuhiro <113022468+fkatsuhiro@users.noreply.github.com>
2026-07-27 01:44:52 +0900

    fix: Deprecated `useAppConfig` in favour of `getAppConfig` (#2487)
    Co-authored-by: Gliches <nishumurmu017@gmail.com>
    Co-authored-by: Aaron <aaronklinker1@gmail.com>

--- packages/wxt/src/utils/app-config.ts
+++ packages/wxt/src/utils/app-config.ts
@@ -15,8 +15,16 @@ export function getAppConfig(): WxtAppConfig {
 /**
  * Alias for {@link getAppConfig}.
  *
+ * @deprecated Use {@link getAppConfig} instead. Same function, different name.
  * @see https://wxt.dev/guide/essentials/config/runtime.html
  */
 export function useAppConfig(): WxtAppConfig {
+  // After v1.0 this function will be removed, and migrate to use only `getAppConfig` instead.
+  if (import.meta.env.DEV) {
+    console.warn(
+      '[wxt] `useAppConfig` is deprecated. Please use `getAppConfig` instead.',
+    );
+  }
+
   return getAppConfig();
 }
