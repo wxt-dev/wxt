d30092403ff10a6bf42ccca8c22401361531ee30
Aaron <aaronklinker1@gmail.com>
2026-08-01 17:33:59 -0500

    chore(deps): Inline `normalize-path` (#2557)
    Co-authored-by: Suvesh Moza <hellosuvesh@gmail.com>

--- packages/wxt/tsdown.config.ts
+++ packages/wxt/tsdown.config.ts
@@ -1,10 +1,53 @@
-import { defineConfig, UserConfig } from 'tsdown';
+import { defineConfig, UserConfig, Rolldown } from 'tsdown';
 import pkgJson from './package.json' with { type: 'json' };
 import { readFile, writeFile } from 'node:fs/promises';
 import {
   virtualEntrypointModuleNames,
   virtualModuleNames,
 } from './src/core/utils/virtual-modules';
+import consola from 'consola';
+import { styleText } from 'node:util';
+import { resolve } from 'node:path';
+
+// Before doing the actual build, "inline" some dependencies by bundling them
+// individually to ESM and then, when building WXT, resolve them from the
+// bundled local versions instead of listing them as a package dependency.
+//
+// Normally, this is exactly what `deps.alwaysBundle` does, but that option
+// doesn't work in combination with "unbundle" mode - which WXT uses so it's
+// `dist/` dir mirrors the `src/` dir, making it easy to explore and patch.
+
+const inlineDeps = ['normalize-path'];
+
+console.log();
+consola.info('Transforming inline dependencies...');
+const res = await Rolldown.build(
+  inlineDeps.map(
+    (id): Rolldown.BuildOptions => ({
+      input: id,
+      output: {
+        dir: 'inline',
+        entryFileNames: `${id}/index.mjs`,
+        format: 'esm',
+        minify: true,
+      },
+      platform: 'node',
+    }),
+  ),
+);
+for (const output of res) {
+  consola.info(`${styleText('dim', 'dist/')}\`${output.output[0].fileName}\``);
+}
+consola.success('Done!');
+console.log();
+
+const resolveInlinePlugin: Rolldown.Plugin = {
+  name: 'resolve-inline',
+  resolveId: {
+    filter: { id: new RegExp(`(${inlineDeps.join('|')})`) },
+    handler: (id) => resolve(`inline/${id}/index.mjs`),
+  },
+};
 
 export default defineConfig([
   // Non-virtual modules can be transpiled in-place to make debugging in node_modules easier
@@ -23,7 +66,9 @@ export default defineConfig([
     unbundle: true,
     deps: {
       neverBundle: ['wxt/browser', 'virtual:app-config'],
+      alwaysBundle: inlineDeps,
     },
+    plugins: [resolveInlinePlugin],
     copy: [
       // If tsdown bundles this file, it removes the triple-slash reference, so
       // we need to copy it into the out dir manually instead of building it.
